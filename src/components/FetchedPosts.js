21aba5ef59d66bad8d337b38732d64357c39cbd9
talgat55 <lightxdesign55@gmail.com>
2020-03-23 00:34:06 +0600

    add show hide loader

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Post from "./Post";
 import {useDispatch,useSelector} from "react-redux";
 import {fetchPost} from "../redux/actions";
+import {Loader} from "./Loader";
 
 export default () => {
     const dispatch = useDispatch();
     const posts = useSelector(state => state.posts.fetchedPosts);
+    const loading = useSelector(state => state.app.loading);
+
+    if(loading){
+        return <Loader/>;
+    }
 
     if (!posts.length){
         return <button
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import {CREATE_POST, FETCH_POST} from "./types";
+import {CREATE_POST, FETCH_POST, HIDE_LOADER, SHOW_LOADER} from "./types";
 
 export function createPost(post) {
     return {
@@ -7,10 +7,23 @@ export function createPost(post) {
     }
 }
 
+export function showLoader() {
+    return{
+        type: SHOW_LOADER
+    }
+}
+export function hideLoader() {
+    return{
+        type: HIDE_LOADER
+    }
+}
+
 export function fetchPost() {
     return  async dispatch => {
+        dispatch(showLoader());
         const response = await fetch('https://jsonplaceholder.typicode.com/posts?_page=1');
         const json = await response.json();
-        dispatch({type: FETCH_POST, payload: json})
+        dispatch(hideLoader());
+        dispatch({type: FETCH_POST, payload: json});
     }
 }
